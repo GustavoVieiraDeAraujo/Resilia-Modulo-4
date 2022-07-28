71606f547d08a724a5ac0c5031fc2ceafbfbe972
Gustavo Vieira de Araújo <gustavova2003@hotmail.com>
2022-07-28 14:40:56 -0300

    Mini projeto finalizado com sucesso

---
+++ Mini Projeto(semana 1 e 2).js
@@ -0,0 +1,29 @@
+// Necessario ter Node.js instalado;
+// Necessario instalar pacote 'readline-sync' do Node.js;
+// Para instalar o pacote rode o seguinte comando:
+// npm install readline-sync 
+
+
+import leitor from "readline-sync"
+
+let propriedadeCSS = "iniciar"
+let listaPropriedadesCSS = []
+
+while (propriedadeCSS !== "Sair"){
+  propriedadeCSS = leitor.question('Digite uma propriedade CSS ou digite "Sair" para mostrar as propriedades ordenas de forma alfabetica e encerrar o programa: ')
+  const promissePropriedadeCSS = new Promise ((resolve,reject)=> {
+    (propriedadeCSS !== "Sair") ? resolve(propriedadeCSS) : reject()
+  })
+  .then(resultadoPromisse => {
+    listaPropriedadesCSS.push(resultadoPromisse)
+  })
+  .catch(resultadoPromisse => {
+    listaPropriedadesCSS.sort()
+    console.log()
+    console.log("Propriedades ordenadas de forma alfabetica")
+    console.log("↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ")
+    for(let i = 0; i<listaPropriedadesCSS.length; i++){
+      console.log(""+listaPropriedadesCSS[i])
+    }
+  })   
+}
